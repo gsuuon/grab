8f46ac7f114f809c7646847e8532a25fc78f5d08
Steven Sun <gsuuon@gmail.com>
2023-05-01 17:18:04 -0500

    feat: choose region

--- Program.fs
+++ Program.fs
@@ -10,11 +10,19 @@ open Gsuuon.Console.Choose
 let run out err (p: Proc) =
     p <!> Stdout |> consume out
     p <!> Stderr |> consume err
-
     p |> wait |> ignore
 
 let hide = run ignore ignore
-let show = run (printfn "%s") (eprintfn "%s")
+let show = run (printf "%s") (eprintf "%s")
+
+let debugMode =
+    if Environment.GetEnvironmentVariable "DEBUG" = "true" then
+        printfn "Debug mode"
+        true
+    else
+        false
+
+let exec = if debugMode then show else hide
 
 setupConsole ()
 
@@ -44,10 +52,12 @@ Directory.CreateDirectory outputDir |> ignore
 // ffmpeg cli command to record desktop and audio on windows
 // ffmpeg -f gdigrab -framerate 30 -i desktop -f dshow -i audio="virtual-audio-capturer" output.mp4
 
-let dshowDevices =
+let lines (x: string) = x.Split "\n"
+
+let dshowDevicesAudio =
     proc "ffmpeg" "-list_devices true -f dshow -i dummy" |> wait <!> Stderr
     |> readBlock
-    |> fun s -> s.Split "\n"
+    |> lines
     |> Array.choose (fun l ->
         // slogn [bg Color.Red] l
         let m = Regex.Match(l, """dshow.+] "(?<name>.+)" \((?<type>.+)\)""")
@@ -65,19 +75,34 @@ let dshowDevices =
             None
     )
 
-let doRecord audioIn =
+let wmicDisplayResolutions =
+    proc "wmic" "path Win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution"
+    |> wait <!> Stdout |> readBlock |> lines
+    |> Array.choose (fun x ->
+            let m = Regex.Match(x, "(?<width>\d+)\s+(?<height>\d+)")
+            if m.Success then
+                Some $"""{m.Groups["width"]}x{m.Groups["height"]}"""
+            else
+                None
+       )
+    |> Array.toList
+
+let doRecord videoRegion audioIn =
     eprintfn "Recording in 3.."
     sleep 1000
-
     eprintfn "2.."
     sleep 1000
     eprintfn "1.."
     sleep 1000
     eprintfn "🎬 (ctrl-c to stop)"
 
-    proc
-        "ffmpeg"
-        ("-f gdigrab -framerate 30 -i desktop "
+    let args = 
+        ( "-f gdigrab -framerate 30 "
+        + ( match videoRegion with
+            | Some reg -> $"-video_size {reg} -show_region 1 "
+            | _ -> ""
+          )
+        + "-i desktop "
         + ( match audioIn with
             | Some source -> $"""-f dshow -i audio="{source}" """
             | _ -> ""
@@ -85,7 +110,10 @@ let doRecord audioIn =
         + """-filter_complex "scale=-2:800:flags=lanczos" -c:v libx264 -preset veryfast -crf 23 -pix_fmt yuv420p -movflags +faststart -y """
         + tmpFilePath
         )
-    |> hide
+
+    if debugMode then printfn "ffmpeg args: %s" args
+
+    proc "ffmpeg" args |> exec
 
     eprintfn "✂️"
 
@@ -100,17 +128,28 @@ let doRecord audioIn =
     eprintfn $"Trimming last {trimMs}ms"
 
     proc "ffmpeg" $"-i {tmpFilePath} -to {endtime} -y -c copy {outputFilePath}"
-    |> hide
+    |> exec
 
     printfn "%s" outputFilePath
 
-dshowDevices
-|> Array.choose (fun x ->
-    if x.``type`` = "audio" then
-        Some x.name
-    else
-        None
-)
-|> Array.toList
-|> choose "Choose audio input (esc or ctrl-c for none):\n" 0
-|> doRecord
+let videoRegion =
+    wmicDisplayResolutions
+    |> choose "Pick a region to capture or esc for entire desktop" 0
+
+let audioIn =
+    dshowDevicesAudio
+    |> Array.choose (fun x ->
+        if x.``type`` = "audio" then
+            Some x.name
+        else
+            None
+    )
+    |> Array.toList
+    |> choose "Choose audio input (esc or ctrl-c for none):\n" 0
+
+eprintfn "Region: %A" videoRegion
+eprintfn "Audio: %A" audioIn
+
+match choose "Ready to record" 0 [ "Start" ] with
+| Some _ -> doRecord videoRegion audioIn
+| None -> eprintfn "Ok, nevermind"

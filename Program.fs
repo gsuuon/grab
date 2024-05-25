4c226dcf7ab6d86edfac43bdd0cda3c289cc0fdc
Steven Sun <gsuuon@gmail.com>
2024-05-24 19:28:31 -0500

    feat: add webm format as default, tweak quality

--- Program.fs
+++ Program.fs
@@ -8,6 +8,9 @@ open Gsuuon.Command.Utility
 open Gsuuon.Console.Choose
 open Gsuuon.Tool.Grab.Ffmpeg
 
+if not (Application.SetHighDpiMode HighDpiMode.PerMonitorV2) then
+    eprintfn "Failed to set high dpi mode, video regions may be off"
+
 let run out err (p: Proc) =
     p <!> Stdout |> consume out
     p <!> Stderr |> consume err
@@ -57,9 +60,13 @@ let dshowDevicesAudio () =
         else
             None)
 
+[<RequireQualifiedAccess>]
+type Format = mp4 | webm
+
 type Options =
     { outputFile: string
       outputDir: string
+      outputFormat: Format
       videoRegion: Region option
       audioIn: string option
       audioDelay: float option
@@ -69,15 +76,17 @@ let doRecord (options: Options) =
     Directory.CreateDirectory options.outputDir |> ignore
 
     // We write a temporary recording so we can trim it
-    let tmpFilePath = Path.Combine [| options.outputDir; "__raw_last_recording.mp4" |]
+    let tmpFilePath = 
+        Path.Combine [|
+            options.outputDir
+            "__raw_last_recording"
+        |] + "." + options.outputFormat.ToString()
 
     let outputFilePath =
-        let path = Path.Combine [| options.outputDir; options.outputFile |]
-        // TODO I'm assuming the preset requires mp4 so we do this to avoid
-        // having to add mp4 to filename will want to change this if we
-        // allow configuring output filetype
-
-        if path.EndsWith ".mp4" then path else path + ".mp4"
+        Path.Combine [|
+            options.outputDir
+            options.outputFile
+        |] + "." + options.outputFormat.ToString()
 
     ConsoleSetup.interruptOnCtrlC ()
 
@@ -91,14 +100,21 @@ let doRecord (options: Options) =
 
     ConsoleSetup.passthroughCtrlC ()
 
-    let small = """-filter_complex "scale=-2:800:flags=lanczos" -c:v libx264 -preset fast -crf 26 -pix_fmt yuv420p -r 30 -y"""
+    let small = """-filter_complex "scale=-2:800:flags=lanczos" """
+    let mp4 = "-c:v libx264 -preset fast -crf 26 -pix_fmt yuv420p -r 24 -y"
+    let webm ="-c:a libvorbis -c:v libvpx-vp9 -preset fast -crf 36 -pix_fmt yuv420p -r 24 -y"
+
+    let formatArgs =
+        match options.outputFormat with
+        | Format.mp4 -> small + mp4 
+        | Format.webm -> small + webm
 
     ffmpeg
         [ Nostdin
           RawArg "-use_wallclock_as_timestamps 1"
           RawArg "-thread_queue_size 64"
           Gdigrab
-              { framerate = 30
+              { framerate = 24
                 frame =
                   match options.videoRegion with
                   | Some region ->
@@ -117,7 +133,8 @@ let doRecord (options: Options) =
           | Some amt -> RawArg (sprintf "-af asetpts=PTS+%f/TB" amt)
           | None -> ()
 
-          RawArg small
+          RawArg formatArgs
+
           RawArg """-movflags +faststart""" ]
 
         (if options.trimLastSecond then
@@ -144,15 +161,18 @@ let doRecord (options: Options) =
 
     printfn "%s" outputFilePath
 
+let screenToRegion (screen: Screen) = {
+    width = screen.Bounds.Width
+    height = screen.Bounds.Height
+    offsetX = screen.Bounds.X
+    offsetY = screen.Bounds.Y
+}
+    
 module Pick =
+
     let videoRegion () =
         Screen.AllScreens
-        |> Array.map (fun x -> {
-                width = x.Bounds.Width
-                height = x.Bounds.Height
-                offsetX = x.Bounds.X
-                offsetY = x.Bounds.Y
-            })
+        |> Array.map screenToRegion
         |> Array.toList
         |> choose "Pick a region to capture or esc for entire desktop" 0
 
@@ -166,17 +186,18 @@ let help =
     """USAGE: grab [options] [<file>]
 
 OUTPUT:
-    [<file>]          set output filename (defaults to 'output')
+    [<file>]          set output filename without extension (defaults to 'output')
 
 OPTIONS:
     -d <path>         set output directory (defaults to '~/recordings')
-    -a                pick an audio source
+    -a                pick an audio source (no audio without)
     --delay <float>   audio delay
     -v                pick a video region
+    -m                output mp4 file (defaults to webm without)
     -T                disable trimming last second
 """
 
-let showHelpAndExit =
+let showHelpAndExit () =
     eprintf "%s" help
     exit 0
 
@@ -186,10 +207,10 @@ let parseOptions args =
     let parseSwitch (options: Options) switch =
         match switch with
         | 'a' -> { options with audioIn = Pick.audioIn () }
-        | 'h' ->
-            showHelpAndExit
+        | 'h' -> showHelpAndExit ()
         | 'v' -> { options with videoRegion = Pick.videoRegion () }
         | 'T' -> { options with trimLastSecond = false }
+        | 'm' -> { options with outputFormat = Format.mp4 }
         | _ ->
             eprintfn "Unknown switch %c" switch
             options
@@ -209,7 +230,7 @@ let parseOptions args =
 
             parse rest options'
         | "--help" :: rest ->
-            showHelpAndExit
+            showHelpAndExit ()
         | switches :: rest when switches.StartsWith("-") ->
             let parsedSwitches = switches.Substring(1) |> Seq.fold parseSwitch options
 
@@ -222,12 +243,13 @@ let parseOptions args =
             parse rest options
 
     parse args
-        { outputFile = "output.mp4"
+        { outputFile = "output"
+          outputFormat = Format.webm
           outputDir =
             Path.Combine
                 [| Environment.GetFolderPath Environment.SpecialFolder.UserProfile
                    "recordings" |]
-          videoRegion = None
+          videoRegion = Some(Screen.PrimaryScreen |> screenToRegion)
           audioIn = None
           audioDelay = None
           trimLastSecond = true }
